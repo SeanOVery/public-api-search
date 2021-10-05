115be5c721372aa1fedc7e01715cb87b8db3dd5e
SeanOVeryMac <seanovery1@gmail.com>
2021-10-04 20:14:54 -0600

    selecting api to propagate dropdown set up but still needs api types  other than art set up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 const formEl = document.querySelector('.search-form'),
   apiTypeSearch = document.querySelector('.text'),
-  apiSelect = document.querySelector('#api-select-dropdown')
+  apiSelect = document.querySelector('#api-select-dropdown'),
+  apiTypeSelect = document.querySelector('.api-type-select'),
+  artArr = ['Art Institute of Chicago', 'Colour Lovers', 'Icon Horse', 'Metropolitan Museum of Art', 'PHP-Noise', 'Pixel Encounter'],
+  artArrShort = ['artchicago', 'colourovers','iconhorse','metart', 'phpnoise','pixelencounter']
 
-let redirectURL = `./secondpage.html?`
+let redirectURL = `./secondpage.html?`,
+  arr,
+  arrShort
 
 
 $('.ui.dropdown')
@@ -16,3 +21,33 @@ formEl.addEventListener('submit', (ev) => {
     apiSelection = apiSelect.value
   location.href = redirectURL + `apitype=${apiType}&apiselection=${apiSelection}`
 })
+
+function setDropdown(event) {
+  let target = event.target,
+    val
+  if (target.hasAttribute('data-select')) {
+    val = target.innerText.trim()
+  }
+
+  if (val === 'Art') {
+      arr = artArr
+      arrShort = artArrShort
+      dropdownCreation()
+  }
+}
+
+function dropdownCreation() {
+  while (apiSelect.firstChild) {
+    apiSelect.removeChild(apiSelect.firstChild)
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let newOption = document.createElement('option')
+
+    newOption.setAttribute('value', arrShort[i])
+    newOption.textContent = arr[i]
+    apiSelect.append(newOption)
+
+  }
+}
+
+apiTypeSelect.addEventListener('click', setDropdown)
