740dca019a17fa188cc1030a26023850655b5bdc
SeanOVeryMac <seanovery1@gmail.com>
2021-10-08 14:48:46 -0400

    library of congress done and added 500 server error handling since openlibrary is ded rn

--- assets/js/second.js
+++ assets/js/second.js
@@ -1,6 +1,7 @@
 const apiCall = document.querySelector('#api-call'),
   results = document.querySelector('.results-container'),
-  recentSearchEl = document.querySelector('#recent-search-dropdown')
+  recentSearchEl = document.querySelector('#recent-search-dropdown'),
+  errorDialogEl = document.querySelector('#dialog')
 
 let recentSearchArr = [],
   queryStr = document.location.search.split('&'),
@@ -34,15 +35,89 @@ const booksApiFunc = () => {
   if (apiToFetch === 'britNatBiblio') {
 
   } else if (apiToFetch === 'libOfCongress') {
+    callUrl = 'https://www.loc.gov/search/?q=baseball&fo=json'
+    fetch(callUrl)
+    .then(response => {
+      if (response.status === 200) {
+      return response.json()
+      } else if (response.status === 500) {
+        throw new Error('Server Error: 500')
+      }
+    })
+      .then(data => {
+        searchData = data
+      apiCall.innerHTML = `   
+        <h1>Example Api Call</h1>   
+        <div class="ui grid">
+        <div class="four wide column left">
+          <img src="${searchData.results[0].image_url[0]}">
+        </div>
+        <div class="twelve wide column right">
+        <table class="ui attached inverted table center aligned">
+          <thead>
+            <th>${searchData.results[0].title}</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Link to LOC article: <a target="_blank" href="${searchData.results[0].url}">${searchData.results[0].title}</a></td>
+            </tr>
+            <tr>
+              <td>Whether article is fully digitized: ${searchData.results[0].digitized}</td>
+            </tr>
+              <td>Subjects this article covers: ${searchData.results[0].subject[0]}, ${searchData.results[0].subject[1]}</td>
+          </tbody>
+        </table> 
+        </div>
+        </div>`
+     
+      results.innerHTML = `<div class="ui top attached inverted segment center aligned">
+        Types of Information you can gather from the library of congress
+      </div>
+      <table class="ui attached inverted table center aligned">
+        <thead>
+          <th>Library of Congress(General Search)</th>
+          <th>Library of Congress(Search by Format)</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>General search provides a very wide range of information including the following and more</td>
+            <td>Search allowing specification by some of the following formats</td>
+          </tr>
+          <tr>
+            <td>Link to article on LOC website</td>
+            <td>Maps, Documents</td>
+          </tr>
+          <tr>
+            <td>Subjects the article covers</td>
+            <td>Audio Files, Films, Sheet Music</td>
+          </tr>
+          <tr>
+            <td>Whether the article is fully digitized</td>
+            <td>Archived Websites</td>
+          </tr>
+        </tbody>
+      </table>`
+      })
+      .catch(error => {
+        errorDialogEl.textContent = error
+        $('#dialog').dialog({
+          height: 100,
+          width: 200,
+        })
+      })
+
 
   } else if (apiToFetch === 'openLibrary') {
     callUrl = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings'
     fetch(callUrl)
       .then(response => {
+        if (response.status === 200) {
         return response.json()
+        } else if (response.status === 500) {
+          throw new Error('Server Error: 500')
+        }
       })
       .then(data => {
-        console.log(data)
         searchData = data
       apiCall.innerHTML = `   
         <h1>Example Api Call</h1>   
@@ -99,6 +174,13 @@ const booksApiFunc = () => {
         </tbody>
       </table>`
       })
+      .catch(error => {
+        errorDialogEl.textContent = error
+        $('#dialog').dialog({
+          height: 100,
+          width: 200,
+        })
+      })
 
   } else {
 
@@ -118,4 +200,3 @@ const checkApiType = () => {
 }
 
 init()
-
